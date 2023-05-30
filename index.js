7a68c214e069fd414163e5e20443aa1035fac47d
Richard Quizon <codenomics@proton.me>
2023-05-30 00:20:58 -0700

    final touches on the main criteria

--- index.js
+++ index.js
@@ -5,8 +5,22 @@ const pc = newPlayableCharacter(100, 110)
 const npc = newNonPlayableCharacter(50, 300)
 
 // have the NPC start walking east immediately
-npc.walkEast()
+// set 8000 as the parameter so that the npc would walk to the same distance as shown in the gif example on the assignment. Placed this console.log() so I could see the results on the browser console and this helped me to troubleshoot my initial issues getting the npc to do what was required.
 
+// Update: Finally was able to call this function correctly with all the images showing. The Red NPC will move after invoking moveNPC(); This now gives the directions on what the NPC will do while following the funtional behavior we created in newNonPlaybleCharacter.js
+
+async function moveNPC(){
+    await npc.walkNorth(1400);
+    await npc.walkEast(1200);
+    await npc.walkSouth(300);
+    await npc.walkEast(1500);
+    await npc.walkSouth(1500);
+    await npc.walkWest(2700);
+    await npc.walkNorth(400);
+}
+
+moveNPC();
+                        
 // Create the inventory
 const inventory = newInventory()
 move(inventory).to(0, 0)
--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -19,41 +19,62 @@ function newNonPlayableCharacter(x, y) {
         }
         element.style.left = x + 'px'
         element.style.bottom = y + 'px'
-    }
+}
 
     setInterval(moveCharacter, 1)
 
-    function walkEast() {
-        direction = 'east'
-        element.src = `./assets/red-character/east.gif`
-    }
+// Created the async funtion and the await on sleep() here. Had some trouble trying to get the red-character to walk east. Either it did the pause first and then walked east to go off screen or it wouldn't walk when I placed the direction to null. Figured out how to let it walk east first, then it would stop from the npc.walkEast().then and then it invoked the number parameter I set on index.js.
 
-    function walkNorth() {
-        direction = 'north'
-        element.src = `./assets/red-character/north.gif`
-    }
+// Update: Improved the async function so that it defines the functionality of the NPC a bit better. Now calling the function for index.js utilizes the behaviors that the NPC is allowed from the code below.
 
-    function walkWest() {
-        direction = 'west'
-        element.src = `./assets/red-character/west.gif`
-    }
+async function sleepAndStop(time) {
+    await sleep(time);
+    stop();
+}
 
-    function walkSouth() {
-        direction = 'south'
-        element.src = `./assets/red-character/south.gif`
-    }
+async function walkNorth(time) {
+    direction = 'north'
+    element.src = `./assets/red-character/north.gif`
+    await sleepAndStop(time);
+}
 
-    function stop() {
-        direction = null
-        element.src = `./assets/red-character/static.gif`
-    }
+async function walkWest(time) {
+    direction = 'west'
+    element.src = `./assets/red-character/west.gif`
+    await sleepAndStop(time);
+}
 
-    return {
-        element: element,
-        walkWest: walkWest,
-        walkNorth: walkNorth,
-        walkEast: walkEast,
-        walkSouth: walkSouth,
-        stop: stop
-    }
+async function walkSouth(time) {
+    direction = 'south'
+    element.src = `./assets/red-character/south.gif`
+    await sleepAndStop(time);
+}
+
+async function walkEast(time) {
+    direction = 'east'
+    element.src = `./assets/red-character/east.gif`
+    await sleepAndStop(time);
+}
+
+function stop() {
+    direction = null
+    element.src = `./assets/red-character/static.gif`
+}
+
+return {
+    element: element,
+    walkWest: walkWest,
+    walkNorth: walkNorth,
+    walkEast: walkEast,
+    walkSouth: walkSouth,
+    stop: stop
+}
+}
+
+// Adding sleep function that will return a promise. Per the instructions will add here.
+// Further, the setTimeout will accept a callback and the main function now returns a promise.
+function sleep(time) {
+    return new Promise (resolve => {
+        setTimeout(resolve, time)
+    });
 }
